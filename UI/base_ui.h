0cd6021c77414bf5684ff1c2660610ba88465e5b
Manoj2210 <manojkumarmaths1000@gmail.com>
2019-01-29 00:41:12 +0530

    UI: Add functiokns for basic game play

--- UI/base_ui.h
+++ UI/base_ui.h
@@ -6,15 +6,19 @@
 #include<QVector3D>
 #include<QParallelAnimationGroup>
 #include<QSequentialAnimationGroup>
+#include"../core/GameManager.h"
+#include"token.h"
 
 class Base_ui: public QObject
 {
     Q_OBJECT
 
 public:
-     explicit Base_ui();
+    Base_ui(GameManager &);
 
     void Setup_ui();
+
+    void tokenFunction();
 
     void Players_ui_creater();
 
@@ -23,6 +27,15 @@ public:
     void Player_animation(QVector3D Final_pos, int Player_number );
 
     void Rotation_Player(int degree,int Player_Number);
+
+
+
+
+    QVector3D function_getpos();
+     QVector3D function_getview();
+
+     void function_setpos(QVector3D,QVector3D);
+
     ~Base_ui();
 
      Qt3DExtras::Qt3DWindow *view;
@@ -30,8 +43,10 @@ private:
     Resources_ui *Prison,*Ufo,*Stadium,*Nuclear,*Buildingnearnuclear,*Buildingnearstadium,*smallchest,*chest,*Buildingnearprison,*buldingnearchest,*buldingneartrain;
     Resources_ui *City;
     Resources_ui *Player[2];
+    Resources_ui *Board[34];
 private slots:
-    void fun();
+    void Focus_fun();
+    void Animation_fun();
 private:
 
     Qt3DCore::QEntity *sceneRoot ;
@@ -41,9 +56,10 @@ private:
     Qt3DExtras::QFirstPersonCameraController *camController;
     QVector3D Player_Position[2],Camera_Position[2],Camera_Viewcenter[2];
     QPropertyAnimation * Playeranimation,* Cameraanimation,* Cameraanimation1,*Rotationplayer,*Rotationcamera,*Rotationcamera1;
-    QTimer timer;
-    int count;
+    int count,Position,player;
     QSequentialAnimationGroup *Animation;
+    Token *token;
+    GameManager *Game;
 };
 
 #endif // BASE_UI_H
